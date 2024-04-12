c2d7403f023042030702956a95290efe25b85f02
TwoUfo <94078255+TwoUfo@users.noreply.github.com>
2024-04-12 16:23:22 +0300

    added registration

--- registration/views.py
+++ registration/views.py
@@ -1,29 +1,33 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
+from .models import CustomUser
 
 
 def login_view(request):
     if request.method == 'POST':
-        username = request.POST["username"]
+        email = request.POST["email"]
         password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username=email, password=password)
         if user is not None:
             login(request, user)
         else:
             error_message = "Invalid username or password."
             return None
 
-        return HttpResponse(request, {"username": username, "password": password})
+        return HttpResponse(request, {"email": email, "password": password})
 
 
 def logout_view(request):
     logout(request)
 
 
-def registration(request):
-    pass
+def register(request):
+    if request.method == 'POST':
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        user = CustomUser.objects.create_user(email=email, password=password)
+
+    else:
+        return HttpResponse(request)
