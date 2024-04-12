e7b3b40744b36f94ec0b90fa40ad898746f19e57
Vic K. <kondratskayavictoria@gmail.com>
2024-04-12 15:07:08 +0300

    made login_view and logout_view

--- registration/views.py
+++ registration/views.py
@@ -3,8 +3,26 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def login(request):
-    pass
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+        else:
+            error_message = "Invalid username or password."
+            return None
+
+        return HttpResponse(request, {"username": username, "password": password})
+
+
+def logout_view(request):
+    logout(request)
 
 
 def registration(request):
