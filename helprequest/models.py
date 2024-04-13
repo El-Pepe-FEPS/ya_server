57b9b5d47a50774a9db8699ebb48e2dcf551e39e
Vic K <kondratskayavictoria@gmail.com>
2024-04-13 18:48:04 +0300

    renamed fields for clarity

--- helprequest/models.py
+++ helprequest/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
 
 
 class HelpRequest(models.Model):
-    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
