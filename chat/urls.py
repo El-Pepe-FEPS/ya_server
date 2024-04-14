0071318b9de147f0ca9b67c4c2d693266ee408e0
NikitaPash <pashchuknik@gmail.com>
2024-04-14 10:09:09 +0300

    Refined chat feature

--- chat/urls.py
+++ chat/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('post/chat/<int:post_id>/', views.ChatView.as_view(), name='chat'),
-    path('chats/', views.AllChatView.as_view(), name='all_chats'),
+    path('chat/<int:post_id>/', views.ChatView.as_view(), name='create_chat'),
+    path('chat/all/', views.AllChatView.as_view(), name='all_chats'),
+    path('message/<int:post_id>/<int:chat_id>/', views.MessageView.as_view(), name='messages'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -14,36 +14,42 @@ class ChatView(APIView):
             post = Post.objects.get(id=post_id)
         except Post.DoesNotExist:
             return Response({"error": "Post not found"}, status=404)
-        chat = Chat.objects.filter(post=post_id).first()
-        if not chat:
-            if post.user_id != request.user.id:
+        if post.user_id != request.user.id:
+            chat = Chat.objects.filter(sender=request.user.id, recipient=post.user_id).first()
+            if not chat:
                 serializer = ChatSerializer(data={
                     'sender': request.user.id,
                     'recipient': post.user_id,
                     'post': post.id
                 }, context={'request': request})
             else:
-                return Response({"message": "You cannot create a chat for your own post"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"message": "Your chat already exists"},
+                                status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"message": "You cannot create a chat for your own post"},
+                            status=status.HTTP_400_BAD_REQUEST)
 
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-            return Response(serializer.data)
-        elif chat:
-            serializer = MessageSerializer(data={
-                'user': request.user.id,
-                'content': request.data['content'],
-                'chat': {'id': chat.id, 'post': post_id}
-            })
+        return Response(serializer.data)
 
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(serializer.data)
 
-    def get(self, request, post_id):
-        queryset1 = Chat.objects.filter(post=post_id).first()
-        queryset2 = Message.objects.filter(chat=queryset1.id).all()
-        serializer = MessageSerializer(queryset2, many=True)
+class MessageView(APIView):
+    def post(self, request, post_id, chat_id):
+        serializer = MessageSerializer(data={
+            'user': request.user.id,
+            'content': request.data['content'],
+            'chat': {'id': chat_id, 'post': post_id}
+        })
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def get(self, request, post_id, chat_id):
+        queryset = Message.objects.filter(chat=chat_id).all()
+        serializer = MessageSerializer(queryset, many=True)
         return Response(serializer.data)
 
 
@@ -51,50 +57,7 @@ class AllChatView(APIView):
     def get(self, request):
         queryset = Chat.objects.filter(Q(sender=request.user) | Q(recipient=request.user)).all()
 
-
         queryset = queryset.distinct()
 
         serializer = ChatSerializer(queryset, many=True)
         return Response(serializer.data)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # try:
-        #     post = Post.objects.get(pk=post_id)
-        # except Post.DoesNotExist:
-        #     return Response({"message": "Post does not exist"}, status=status.HTTP_404_NOT_FOUND)
-        #
-        # chat = Chat.objects.create(post=post)
-        #
-        # message_data = {
-        #     'user': request.user.id,
-        #     'content': request.data.get('content'),
-        #     'chat': chat.id
-        # }
-        # message_serializer = MessageSerializer(data=message_data)
-        # if message_serializer.is_valid():
-        #     message_serializer.save()
-        #     chat_serializer = ChatSerializer(chat)
-        #     return Response({"message": "Message sent", "chat": chat_serializer.data}, status=status.HTTP_201_CREATED)
-        # return Response(message_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
